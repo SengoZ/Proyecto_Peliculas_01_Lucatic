9d23d0dfbed4bcacd68bd6a3c3edd01d795a01a1
admin <admin@Profesor>
2019-03-04 17:35:50 +0100

    para documentaccion

--- src/control/Main.java
+++ src/control/Main.java
@@ -11,9 +11,7 @@ public class Main {
 
 
 	public static void main(String[] args) throws DAOException, LecturaException, ParseException {
-
 		Arranque menu = new Arranque();
 		menu.menu();
-
 	}
 }
--- src/datos/Datos.java
+++ src/datos/Datos.java
@@ -13,14 +13,14 @@ public class Datos implements I_datos {
 	
 	private static final Logger log = LogManager.getLogger("Datos");
 	
-	//@Override
+	@Override
 	public void altaPeliculas(Peliculas peli) throws DAOException {//es necesario actualizar este metodo.
 		try (Statement stmt = (Statement) Conexion_BBDD_prueba.Conecta_BBDD().createStatement()){
 			System.out.println("-------------------------------------------Errorrrrrrr");
 			String query = "INSERT INTO PELICULAS(NOMBRE, ANIOESTRENO, CATEGORIA) VALUES ('" + peli.getNombre()+"' ,"+peli.getAnioEstreno()+ " ,"+peli.getCategoria()+")";
 			if(stmt.executeUpdate(query) != 1) {
 				System.out.println("Errorrrrrrr");
-				//throw new DAOException("Error añadiendo pelicula");
+				throw new DAOException("Error añadiendo pelicula");
 			}
 		}catch(SQLException se) {
 			se.printStackTrace();
@@ -28,9 +28,9 @@ public class Datos implements I_datos {
 			System.out.println("---"+se.getErrorCode());
 			System.out.println("---"+se.getMessage());
 			log.error("----------------Error SQL en alta peliculas");
-		}/*catch(DAOException se2) {
+		}catch(DAOException se2) {
 			log.error("----------------Error DAO en alta peliculas");
-		}*/
+		}
 		
 	}@Override
     public void bajaPeliculas(int id) throws DAOException {
@@ -100,10 +100,10 @@ public class Datos implements I_datos {
  		}
  	}
  	
- 	//
- 	// Método MASVISTAS: sin parámentros de entrada, devuelve por pantalla un listado de las peliculas
- 	// más vistas por un usuario.
- 	//
+ 	/*
+ 	 * Método MASVISTAS: sin parámentros de entrada, devuelve por pantalla un listado de las peliculas
+ 	 * más vistas por un usuario.
+ 	 */
  	
  	public void masVistas() {
  		try (Statement stmt = (Statement) ConexionBBDD.Conecta_BBDD().createStatement()) {
